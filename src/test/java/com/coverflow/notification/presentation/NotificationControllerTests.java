fc0a8277bdc6221f741092b6f3d447ce4014dbc9
fakerdeft <whakswp123@naver.com>
2024-03-25 08:44:15 +0900

    (#158)feat: 알림 테스트 구현 중입니다.

--- src/test/java/com/coverflow/notification/presentation/NotificationControllerTests.java
+++ src/test/java/com/coverflow/notification/presentation/NotificationControllerTests.java
@@ -1,10 +1,39 @@
 package com.coverflow.notification.presentation;
 
+import com.coverflow.notification.application.NotificationService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 @AutoConfigureRestDocs
 @WebMvcTest(NotificationController.class)
 public class NotificationControllerTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean // NotificationController 가 의존하는 빈을 모킹
+    private NotificationService notificationService;
+
+    @Nested
+    @DisplayName("알림 연결 시")
+    class connectNotification {
+
+        @Test
+        @DisplayName("알림 연결에 성공한다.")
+        void success() {
+            //given
+
+
+            //when
+
+
+            //then
+        }
+    }
 }
