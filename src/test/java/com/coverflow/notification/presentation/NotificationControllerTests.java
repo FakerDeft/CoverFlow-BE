4af65da49186f6caf59be5579c686c39667f30fa
fakerdeft <whakswp123@naver.com>
2024-03-24 23:52:53 +0900

    (#158)fix: 알림 서비스 계층 테스트 파일을 컨트롤러 계층으로 수정했습니다.

---
+++ src/test/java/com/coverflow/notification/presentation/NotificationControllerTests.java
@@ -0,0 +1,10 @@
+package com.coverflow.notification.presentation;
+
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@AutoConfigureRestDocs
+@WebMvcTest(NotificationController.class)
+public class NotificationControllerTests {
+
+}
